df9f455f8fc3f68e5b7c70b751cfc0e235ce947b
Gustavo Westarb <gustavowestarb45@gmail.com>
2020-01-23 23:25:01 +0000

    Iniciando funções

---
+++ Fontes/exercicios-js/functions/paramsVariables.js
@@ -0,0 +1,15 @@
+function sum(){
+    let sum = 0;
+    
+    for (const i in arguments) {
+        sum += arguments[i];
+    }
+    
+    return sum;
+}
+
+console.log(sum());
+console.log(sum(1));
+console.log(sum(1.1, 2.2, 3.3));
+console.log(sum(1.1, 2.2, 'Teste'));
+console.log(sum('a', 'b', 'c'));
