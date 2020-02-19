b48b02cba5d8afe47670992dbf1d3a9ea6282dc8
GustavoWestarb <gustavowestarb45@gmail.com>
2020-02-19 13:20:15 -0300

    Continuando capitulo de array

---
+++ Fontes/exercicios-js/arrays/filter1.js
@@ -0,0 +1,8 @@
+const products = [
+    { name: 'Notebook', price: 2500, fragile: true },
+    { name: 'iPad Pro', price: 4199, fragile: true },
+    { name: 'Cup of glass', price: 12.49, fragile: true },
+    { name: 'Cup of plastic', price: 18.99, fragile: false }
+]
+
+console.log(products.filter())
